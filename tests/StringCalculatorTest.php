2a6863db049486830025449eb16ef118f3a956e5
lukes <lads89@gmail.com>
2021-03-30 22:40:20 +0100

    remove test bodies

--- tests/StringCalculatorTest.php
+++ tests/StringCalculatorTest.php
@@ -11,65 +11,38 @@ class StringCalculatorTest extends TestCase
     
     public function testItEvaluatesEmptyStringAsZero()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertSame(0, $calculator->add(''));
     }
 
     
     public function testItfindsTheSumOfASingleNumber()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertSame(5, $calculator->add('5'));
     }
 
     
     public function testItfindsTheSumOfTwoNumbers()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertSame(10, $calculator->add('5,5'));
     }
 
     
     public function testItfindsTheSumOfAnyNumbers()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertSame(19, $calculator->add('5,5,5,4'));
     }
 
     
     public function testItAcceptsANewlineAsADelimeter()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertSame(10, $calculator->add("5\n5"));
     }
-
     
     public function testNegativeNumbersAreNotAllowed()
     {
-        $calculator = new StringCalculator();
-
-        $this->expectException(InvalidArgumentException::class);
-
-        $calculator->add('5,-4');
     }
 
     
     public function testNumbersGreaterThan1000AreIgnored()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertEquals(5, $calculator->add('5,1001'));
     }
 
     public function testItSupportsCustomDelimeters()
     {
-        $calculator = new StringCalculator();
-
-        $this->assertEquals(20, $calculator->add("//:\n5:4:11"));
     }
 }
